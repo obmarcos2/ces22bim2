b8f68f20e3ffbb839aae3e9c10dc9dfee0ad21ff
obmarcos2 <101301437+obmarcos2@users.noreply.github.com>
2022-07-09 05:45:16 +0000

    Update main.py

--- design_patterns_2/1bridge/main.py
+++ design_patterns_2/1bridge/main.py
@@ -1,2 +1,14 @@
 from interfaces import *
 
+m1 = MotorCombustao(120)
+m2 = MotorEletrico(150)
+# criacao do objeto de interface veiculo com
+# agregacao de motores distintos
+veic = Carro(m1, "sedan")
+veic.buzinar_carro()
+
+veic2 = Carro(m2, "hatch")
+veic2.buzinar_carro()
+veic2.acelerar()
+print(m2.get_vel())
+print(m1.get_vel())
